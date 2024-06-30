43d107ad8c382314041171e2d63ea727dd8031bf
ClementSellyPro <clementselly.pro@gmail.com>
2024-06-30 15:53:53 +0400

    job-list-react - Global style

--- job-list-react/src/App.js
+++ job-list-react/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import JobOffer from './component/JobOffer';
 
 function App() {
   return (
     <div className="App">
-      
+      <JobOffer></JobOffer>
     </div>
   );
 }
---
+++ job-list-react/src/component/JobOffer.jsx
@@ -0,0 +1,37 @@
+import '../style/JobOffer.css';
+import profileTest from "../images/photosnap.svg";
+
+function JobOffer(){
+    return (
+        <div className='JobOffer'>
+            <div className='left-section'>
+                <img src={profileTest} alt='profile test'></img>
+                <div className='job-info'>
+                    <div className='job-info_company'>
+                        <p className='job-info_company-text'>Photosnap</p>
+                        <div className='job-info_company-tags'>
+                            <div className='job-info_company-tag-new'>NEW!</div>
+                            <div className='job-info_company-tag-featured'>FEATURED</div>
+                        </div>
+                    </div>
+                    <div className='job-info_offer-title'>
+                        Fullstack Developer
+                    </div>
+                    <div className='job-info_details'>
+                        <div className='job-info_detail'>1d ago</div>
+                        <div className='job-info_detail'>Full Time</div>
+                        <div className='job-info_detail'>USA only</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='job-info_tags'>
+                <div className='job-info_tag'>Frontend</div>
+                <div className='job-info_tag'>Senior</div>
+                <div className='job-info_tag'>HTML</div>
+            </div>
+        </div>
+    );
+}
+
+export default JobOffer;
